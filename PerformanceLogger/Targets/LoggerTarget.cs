8352d9c01e456dec2ea34ec499ab1cde9eec3f25
Corentin Altepe <corentin.altepe@gmail.com>
2018-12-09 22:36:42 +0100

    Added a basic ILogget to ITarget adapter

---
+++ PerformanceLogger/Targets/LoggerTarget.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.Extensions.Logging;
+using PerformanceLogger.ITargets;
+
+namespace PerformanceLogger.Targets
+{
+    /// <summary>
+    /// Adapts ILogger interface to ITarget to write performance logs to an ILogger
+    /// </summary>
+    public class LoggerTarget : ITarget
+    {
+        private readonly ILogger _logger;
+        public LoggerTarget(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Log(PerformanceResult report)
+        {
+            // Format the entry and log it
+            _logger.LogInformation($"{report.StartDate.ToShortDateString()} {report.StartDate.ToShortTimeString()};{report.EventId};{report.Duration.TotalMilliseconds} ms;");
+        }
+    }
+}

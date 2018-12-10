cfcc992fd558724a6241deb9735045f7039905a9
Corentin <corentin.altepe@gmail.com>
2018-12-10 21:26:58 +0100

    Added a test project (empty) and folder structure to future test projects

--- PerformanceLogger/PerformanceLoggerBuilder.cs
+++ PerformanceLogger/PerformanceLoggerBuilder.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using PerformanceLogger.Targets;
 
+[assembly: InternalsVisibleTo("PerformanceLogger.Test")]
 namespace PerformanceLogger
 {
     /// <summary>
---
+++ Tests/Unit/PerformanceLogger.Test/PerformanceLoggerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PerformanceLogger.Test
+{
+    public class PerformanceLogger
+    {
+        [Fact]
+        public void Start()
+        {
+
+        }
+    }
+}
